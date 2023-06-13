0cafa1e491b7bd8f72f449d5bafec246e9e1e5e4
phamdung2209 <phamdung.22092003@gmail.com>
2023-06-13 07:35:14 +0000

    update

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@
 while true; do
   # Đoạn mã của bạn ở đây
   echo "Running..."
-  sleep 300  # Không có thời gian dừng
+  sleep 100  # Không có thời gian dừng
 done
 
 # Đoạn mã sau khi vòng lặp kết thúc
