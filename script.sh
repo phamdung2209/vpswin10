443c11e6931b140e738531f2b33d362423223f65
phamdung2209 <phamdung.22092003@gmail.com>
2023-06-13 07:07:01 +0000

    update

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Đoạn mã khởi tạo
+
+while true; do
+  # Đoạn mã của bạn ở đây
+  echo "Running..."
+  sleep 300  # Không có thời gian dừng
+done
+
+# Đoạn mã sau khi vòng lặp kết thúc
